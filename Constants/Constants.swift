dbf1d969dd5ee9ff967c73ce8b5a53ae409352f4
Annika Mücke <annika-muecke@outlook.de>
2024-07-12 14:48:49 +0200

    add basic barcode scanner

--- Constants/Constants.swift
+++ Constants/Constants.swift
@@ -8,6 +8,15 @@ struct PermissionConstants {
     static let appSettingsUrl = URL(string: UIApplication.openSettingsURLString)
 }
 
+struct ScannerConstants {
+    enum CodeType {
+        case ean8, qr
+    }
+    static let overlayWidthFactor: CGFloat = 1.5
+    static let barcodeWidthHeightRatio: CGFloat = 2
+    static let defaultWidthHeightRatio: CGFloat = 1
+}
+
 struct StyleConstants {
     static let mainScreenFontSize: CGFloat = 30
     static let mainScreenIconOpacity: Double = 0.3
@@ -16,5 +25,10 @@ struct StyleConstants {
     static let edgePadding: CGFloat = 30
     
     static let alertDuration: Double = 8.0
+    
+    static let overlayOpacity: Double = 0.5
+    static let overlayStrokeWidth: CGFloat = 5
+    static let roundedCornerRadius: CGFloat = 15
+    static let roundedCornerStrokeLength: CGFloat = 15
 }
 
--- Views/BarcodeScannerView.swift
+++ Views/BarcodeScannerView.swift
@@ -1,8 +1,43 @@
 import SwiftUI
+import CodeScanner
 
 struct BarcodeScannerView: View {
+    @State var isPresented = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("scanner view"){
+            isPresented = true
+        }
+        .sheet(isPresented: $isPresented) {
+            BarcodeScannerViewWithOverlay(isPresented: $isPresented)
+        }
+        .interactiveDismissDisabled()
+    }
+}
+
+struct BarcodeScannerViewWithOverlay: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        ZStack{
+            CodeScannerView(codeTypes: [.ean8], simulatedData: "1234567", completion: handleScanResult)
+            //                .padding(.horizontal, 40)
+            //                .frame(width: 350, height: 250)
+            ScanOverlayView(codeType: ScannerConstants.CodeType.ean8)
+        }
+    }
+    
+    func handleScanResult(result: Result<ScanResult, ScanError>){
+        isPresented = false
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: ".")
+            // data validation
+            guard details.count == 1 else { return }
+            // process scanned data
+            // ..
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
     }
 }
 
