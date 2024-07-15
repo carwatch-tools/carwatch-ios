14074014f20f53feb703d5f2bcc6a0667c019f3d
Annika Mücke <annika-muecke@outlook.de>
2024-07-15 17:12:56 +0200

    integrate scanner in main view

--- Views/MainView.swift
+++ Views/MainView.swift
@@ -11,40 +11,45 @@ struct MainView: View {
     @State private var alarmsList: [String] = ["10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "12:00", "13:00", "14:00"]
     @State private var alarmsScanned: [Bool] = [true, false, false, false, false, false, false, false, false]
     
+    @State private var isScannerPresented = false
+    @State private var isScanSuccessful = false
+    
     @State private var showAppInfoDialog = false
     @State private var appVersion: String? = nil
     var body: some View {
         if pvm.permissionData.notificationPermissionGranted && pvm.permissionData.cameraPermissionGranted {
-            if appDelegate.openedFromNotification || avm.isScanRequired() {
-//            if true {
-                BarcodeScannerView().onForeground {
-                    avm.setAlarmTriggered()
+            NavigationStack{
+                TabView(selection: $selectedTab){
+                    WakeupView()
+                        .tabItem {
+                            Label("Wakeup", systemImage: "sun.max")
+                        }.tag(0)
+                        .padding(StyleConstants.edgePadding)
+                    AlarmView(alarmActive: avm.alarm.isActive, alarmTime: avm.alarm.time, alarmsList: $alarmsList, alarmsScanned: $alarmsScanned)
+                        .tabItem {
+                            Label("Schedule", systemImage: "alarm")
+                        }.tag(1)
+                    BedtimeView()
+                        .tabItem {
+                            Label("Bedtime", systemImage: "bed.double")
+                        }.tag(2)
                 }
-            } else {
-                NavigationStack{
-                    TabView(selection: $selectedTab){
-                        WakeupView()
-                            .tabItem {
-                                Label("Wakeup", systemImage: "sun.max")
-                            }.tag(0)
-                            .padding(StyleConstants.edgePadding)
-                        AlarmView(alarmActive: avm.alarm.isActive, alarmTime: avm.alarm.time, alarmsList: $alarmsList, alarmsScanned: $alarmsScanned)
-                            .tabItem {
-                                Label("Schedule", systemImage: "alarm")
-                            }.tag(1)
-                        BedtimeView()
-                            .tabItem {
-                                Label("Bedtime", systemImage: "bed.double")
-                            }.tag(2)
-                    }
-                    .navigationBarTitle(Text(tabTitle))
-                    .toolbar {
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            MainViewToolbarMenu(showAppInfoDialog: $showAppInfoDialog, appVersion: $appVersion)
-                        }
+                .navigationBarTitle(Text(tabTitle))
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        MainViewToolbarMenu(showAppInfoDialog: $showAppInfoDialog, appVersion: $appVersion)
                     }
                 }
             }
+            .onForeground {
+                checkAlarmStatus()
+                avm.setAlarmTriggered()
+            }
+            .sheet(isPresented: $isScannerPresented) {
+                BarcodeScannerViewWithOverlay(isPresented: $isScannerPresented, isSuccessful: $isScanSuccessful)
+                    .interactiveDismissDisabled()
+            }
+            
         } else if pvm.permissionData.notificationPermissionGranted {
             if pvm.permissionData.cameraPermissionDialogHandled {
                 MissingPermissionView(type: PermissionConstants.PermissionType.camera)
@@ -66,6 +71,18 @@ struct MainView: View {
         case 1: return String(localized: "Schedule")
         case 2: return String(localized: "Bedtime")
         default: return String(localized: "Title")
+        }
+    }
+    
+    func checkAlarmStatus() {
+        if appDelegate.openedFromNotification {
+            // unhandled notification is present
+            avm.setAlarmTriggered()
+            isScannerPresented = true
+        }
+        if avm.isScanRequired() {
+            // no successful scan yet
+            isScannerPresented = true
         }
     }
 }
