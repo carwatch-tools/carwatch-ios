6394a057ee60cdedd057773d4accea9ff1eef8b9
Annika Mücke <annika-muecke@outlook.de>
2024-09-03 13:29:38 +0200

    make session state persistent

--- Onboarding/ViewModels/SessionViewModel.swift
+++ Onboarding/ViewModels/SessionViewModel.swift
@@ -2,12 +2,49 @@ import Foundation
 
 class SessionViewModel : ObservableObject {
     
-    enum CurrentState {
+    enum CurrentState: String, Codable {
         case registration, tutorial, studyOngoing
     }
     
-    @Published private var currentState: CurrentState = .registration
-    @Published var isReregistration = false
+    @Published private var currentState: CurrentState = .registration {
+        didSet {
+            saveCurrentState()
+        }
+    }
+    @Published var isReregistration = false {
+        didSet {
+            saveRegistrationStatus()
+        }
+    }
+    
+    let currentStateDataKey = "currentState"
+    let registrationStateDataKey = "isReregistration"
+    
+    init() {
+        getSessionData()
+        print("retrieved session data: \(currentState)")
+    }
+    
+    func saveCurrentState() {
+        print("saved session data: \(currentState)")
+        let rawStateValue = currentState.rawValue
+        UserDefaults.standard.set(rawStateValue, forKey: currentStateDataKey)
+    }
+    
+    func saveRegistrationStatus(){
+        UserDefaults.standard.set(isReregistration, forKey: registrationStateDataKey)
+    }
+    
+    func getSessionData() {
+        isReregistration = UserDefaults.standard.bool(forKey: registrationStateDataKey)
+        guard
+            let rawStateValue = UserDefaults.standard.string(forKey: currentStateDataKey),
+            let savedState = CurrentState(rawValue: rawStateValue)
+        else {
+            return
+        }
+        currentState = savedState
+    }
     
     func getCurrentState() -> CurrentState {
         return currentState
